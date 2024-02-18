1b811b078d8165460df4d15228cf18fb6a6624e8
Shourya Bhardwaj <shouryabhardwaj19@gmail.com>
2024-02-18 18:11:42 +0530

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Titanic Survival Prediction Model",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Made a ML model to predict survival rate in titanic dataset",
+    url: "https://github.com/Shouryabhardwajj/Titanic-Survival-Prediction-Model",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Btecky",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Contributed to Hacktoberfest 2023 open source",
+    url: "https://github.com/Shouryabhardwajj/Btecky",
   },
   {
-    title: "My Resume Site",
+    title: "Coursera applied Data Science C apstone",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Made a repository to help students in IBM data science course",
+    url: "https://github.com/Shouryabhardwajj/coursera-applied-data-science-capstone",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Python project",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A simple python project on data science",
+    url: "https://github.com/Shouryabhardwajj/Python-project-for-Data-Science",
   },
 ];
 
